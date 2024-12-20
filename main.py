927c9fc3f0fe35f932e7ddacfd01c9f118a7ca27
jamal <bank@sberofficial.ru>
2024-12-20 21:21:04 +0300

    Add ability to add and delete inventory

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+from flask_restful import abort
 
 from data import db_session
+from data.inventory import Inventory, InventoryForm
 from data.users import LoginForm, User, RegisterForm
 
 app = Flask(__name__)
@@ -23,6 +25,62 @@ def index():
         return render_template("index.html", title='Главное меню')
     else:
         return redirect("/login")
+
+
+@app.route('/inventory')
+@login_required
+def inventory():
+    session = db_session.create_session()
+    return render_template("inventory.html", title="Список инвентаря",
+                           inventory=session.query(Inventory).all(), admin=current_user.is_admin)
+
+
+@app.route('/inventory_delete/<int:id>')
+@login_required
+def inventory_delete(id):
+    session = db_session.create_session()
+
+    item = session.query(Inventory).filter(Inventory.id == id).first()
+    if item and current_user.is_admin:
+        session.delete(item)
+        session.commit()
+    else:
+        abort(404)
+    return redirect('/inventory')
+
+
+@app.route('/inventory_edit', methods=['GET', 'POST'])
+@login_required
+def inventory_add():
+    session = db_session.create_session()
+
+    form = InventoryForm()
+    if form.validate_on_submit():
+        item = Inventory()
+        item.name = form.name.data
+        item.quantity = form.quantity.data
+        item.state = form.state.data
+        session.add(item)
+        session.commit()
+        return redirect('/inventory')
+
+    return render_template('inventory_edit.html', title='Добавление инвентаря', form=form)
+
+
+# @app.route('/inventory_edit/<int:id>', methods=['GET', 'POST'])
+# @login_required
+# def inventory_edit(id):
+#     session = db_session.create_session()
+#
+#     item = session.query(Inventory).filter(Inventory.id == id).first()
+#     if not item:
+#         abort(404)
+#
+#     form = InventoryForm()
+#     if request.method == 'GET':
+#         form.name = item.name
+#         form.quantity = item.quantity
+#         # TODO: finish editing after I add a state option
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -57,7 +115,7 @@ def register():
                                    message="Пользователь с таким логином уже существует",
                                    form=form)
         return redirect("/login")
-    return render_template('register.html', title='Авторизация', form=form)
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route('/logout')
