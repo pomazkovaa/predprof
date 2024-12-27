cde434acffb09be291590c87800fb077dad8552d
User <1@1>
2024-12-27 15:08:41 +0300

    Revert "Add ability to add and delete inventory"
    This reverts commit 927c9fc3f0fe35f932e7ddacfd01c9f118a7ca27.

--- data/users.py
+++ data/users.py
@@ -6,7 +6,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from data.db_session import SqlAlchemyBase
+from .db_session import SqlAlchemyBase
 
 
 class User(SqlAlchemyBase, UserMixin):
