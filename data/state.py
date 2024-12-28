510effe61ee22b5158070eb611717bcc19a4006f
jamal <bank@sberofficial.ru>
2024-12-28 19:04:57 +0300

    Add state to fill_db.py

--- data/state.py
+++ data/state.py
@@ -6,5 +6,5 @@ from data.db_session import SqlAlchemyBase
 class State(SqlAlchemyBase):
     __tablename__ = "state"
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
--- fill_db.py
+++ fill_db.py
@@ -1,5 +1,6 @@
 import os
 from data import db_session
+from data.state import State
 from data.users import User
 
 
@@ -7,14 +8,19 @@ def main():
     if not os.path.exists("db"):
         os.makedirs("db")
         db_session.global_init("db/base.sqlite")
+        session = db_session.create_session()
 
         user = User()
         user.login = 'admin'
         user.set_password("12345678")
         user.is_admin = True
-
-        session = db_session.create_session()
         session.add(user)
+
+        for name in ['Новый', 'Используемый', 'Сломанный']:
+            state = State()
+            state.name = name
+            session.add(state)
+
         session.commit()
     else:
         print("База уже существует!")
