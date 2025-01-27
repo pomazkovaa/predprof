a31b64df04bc12a1fb12f38c8619415d69639e5b
jamal <bank@sberofficial.ru>
2025-01-27 16:42:27 +0300

    Set up docker

--- data/db_session.py
+++ data/db_session.py
@@ -20,7 +20,7 @@ def global_init(db_file):
     conn_str = 'sqlite:///' + db_file.strip()
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False)
+    engine = sa.create_engine(conn_str, echo=False, pool_size=10, max_overflow=20)
     __factory = orm.sessionmaker(bind=engine)
 
     # noinspection PyUnresolvedReferences
